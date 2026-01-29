dd2f0fdad4f57601342f8042176f1bbfc04dfd44
Chris Rensel-Smith <32113009+0Starus@users.noreply.github.com>
2026-01-29 15:21:57 -0800

    plinko rework

---
+++ Plinko/Assets/BallSpawner.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+using UnityEngine.InputSystem;
+
+public class BallSpawner : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject ballPrefab;
+
+    void Start()
+    {
+        Instantiate(ballPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.isPressed)
+        {
+            Transform myTransform = GetComponent<Transform>();
+            Instantiate(ballPrefab,myTransform.position, Quaternion.identity);
+        }
+    }
+}
+
--- week 1/week 1/Assets/Solar System Textures/doot.cs
+++ week 1/week 1/Assets/Solar System Textures/doot.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class doot : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public float YawSpeedPerSecond = 45;
+    public Transform centerPoint;
+    public Vector3 axis =Vector3.up;
+    public float YawSpeedPerSecond = 45f;
     void Start()
     {
         Debug.Log("Unity Hello World");
@@ -12,6 +14,7 @@ public class doot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.RotateAround(centerPoint.position, axis,YawSpeedPerSecond* Time.deltaTime );
         Transform MyTransform = GetComponent<Transform>();
         MyTransform.Rotate(new Vector3(0f, YawSpeedPerSecond* Time.deltaTime,0f));
     }
