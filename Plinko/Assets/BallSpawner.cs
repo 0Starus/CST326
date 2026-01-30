d942684b464935797e34b13001b7c3693d6022fd
Chris Rensel-Smith <32113009+0Starus@users.noreply.github.com>
2026-01-29 19:50:49 -0800

    pong start

--- Plinko/Assets/BallSpawner.cs
+++ Plinko/Assets/BallSpawner.cs
@@ -6,19 +6,28 @@ public class BallSpawner : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject ballPrefab;
-
+    public int timer;
     void Start()
     {
         Instantiate(ballPrefab);
+        timer = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(timer > 0)
+        {
+            timer--;
+        }
         if (Keyboard.current.spaceKey.isPressed)
         {
-            Transform myTransform = GetComponent<Transform>();
-            Instantiate(ballPrefab,myTransform.position, Quaternion.identity);
+            if(timer == 0)
+            {
+                Transform myTransform = GetComponent<Transform>();
+                Instantiate(ballPrefab,myTransform.position, Quaternion.identity);
+                timer = 100;
+            }
         }
     }
 }
